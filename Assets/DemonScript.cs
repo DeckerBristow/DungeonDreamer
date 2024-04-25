8ef955832b90b999d81e6414830cf27402823ec9
liamrodonnell1 <lro8a@virginia.edu>
2024-04-24 23:42:55 -0400

    adding attack animation

--- Assets/DemonScript.cs
+++ Assets/DemonScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DemonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Rigidbody2D playerRb;
     [SerializeField] private Rigidbody2D demonRb;
     public GameObject gameObjectWithCollider;
@@ -13,7 +12,11 @@ public class DemonScript : MonoBehaviour
     private BoxCollider2D boxCollider;
 
     public int Health = 10;
+    public float attackDistance = 2f;
+
     public Animator animator;
+    private bool isAttacking = false;
+    private bool attackAnimationComplete = true;
 
     void Start()
     {
@@ -21,51 +24,54 @@ public class DemonScript : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate()
     {
         if(animator.GetBool("alive")){
+            if (Mathf.Abs(demonRb.transform.position.x - playerRb.position.x) < attackDistance)
+            {
+                if(demonRb.transform.position.x > playerRb.transform.position.x)
+                {
+                    animator.SetTrigger("Attack");
+                    isAttacking = true;
+                }
+                else
+                {
+                    animator.SetTrigger("Attack_Right");
+                }
+                
+            }
             if (demonRb.transform.position.x > playerRb.transform.position.x){
                 demonRb.transform.Translate((-1*movementSpeed), 0, 0);
                 animator.SetBool("isWalkingLeft", true);
                 animator.SetBool("isWalkingRight", false);
-                
-
             }
             if (demonRb.transform.position.x < playerRb.transform.position.x){
                 demonRb.transform.Translate((movementSpeed), 0, 0);
                 animator.SetBool("isWalkingRight", true);
                 animator.SetBool("isWalkingLeft", false);
-
-
             }
             if (demonRb.transform.position.y > (playerRb.transform.position.y+1)){
                 demonRb.transform.Translate(0, (-1*movementSpeed/2), 0);
-
             }
             if (demonRb.transform.position.y < playerRb.transform.position.y+1){
                 demonRb.transform.Translate(0, (movementSpeed/2), 0);
-
             }
         }
-        
-
     }
+
+    private void AttackAnimationComplete()
+    {
+        attackAnimationComplete = true;
+        isAttacking = false;
+    }
+
     public void DestroyEnemy()
     {
-
         Destroy(gameObject);
-        
     }
 
     public void Died()
     {
-
         boxCollider.enabled = false;
 
         float x = gameObject.transform.position.x;
